2dd787a1d3c6905eb021d05de6f3f6da17c97538
Michael W <gcr@sneakygcr.net>
2011-02-19 19:28:27 -0700

    more stupid hacks upon stupid hacks

--- delay-proxy.js
+++ delay-proxy.js
@@ -7,12 +7,19 @@ var http = require('http');
 // sudo -u nobody node rotating-proxy.js
 
 var al = new (require('./lib/async_lock.js').AsyncLock)(),
-    TIMEOUT=1000 * 20,
+    TIMEOUT=1000 * 30,
     TIMED_DOMAINS = ['hackerne.ws', 'reddit', 'iamlights', 'bofh.ntk.net'];
 
-function shouldBeTimed(host) {
+function shouldBeTimed(req) {
+  if (req.url.indexOf("png") != -1 ||
+      req.url.indexOf("css") != -1 ||
+      req.url.indexOf("jpg") != -1 ||
+      req.url.indexOf("ico") != -1 ||
+      req.url.indexOf("gif") != -1) {
+    return false;
+  }
   for (var target=0,l=TIMED_DOMAINS.length; target<l; target++) {
-    if (host.toLowerCase().indexOf(TIMED_DOMAINS[target]) != -1) {
+    if (req.headers.host.toLowerCase().indexOf(TIMED_DOMAINS[target]) != -1) {
       return true;
     }
   }
@@ -24,19 +31,29 @@ http.createServer(function(proxiedReq, proxiedRes) {
   // Make the outgoing connection
   console.log("Waiting...");
   function serviceRequest(cc, timeout) {
+    // call cc when the page is loaded.
+    // WARNING cc may be called multiple times.
+    var cont = true;
+    proxiedReq.connection.setMaxListeners(15);
+    proxiedReq.connection.addListener('error', function(error) {
+        console.log(error);
+        console.log(error.stack);
+        cont=false;
+        cc();
+      });
+    proxiedReq.connection.addListener('end', function(error) {
+        console.log("Canceling request");
+        cont=false;
+        cc();
+      });
     setTimeout(function(){
+      if(!cont) { return; }
       console.log("Serving request...");
       delete proxiedReq.headers.connection;
       var newClient = http.createClient(80, proxiedReq.headers.host),
           newReq = newClient.request(proxiedReq.method, proxiedReq.url, proxiedReq.headers);
 
       newClient.addListener('error', function(error) {
-          console.log(error);
-          console.log(error.stack);
-          cc();
-        });
-      proxiedReq.connection.setMaxListeners(15);
-      proxiedReq.connection.addListener('error', function(error) {
           console.log(error);
           console.log(error.stack);
           cc();
@@ -76,7 +93,8 @@ http.createServer(function(proxiedReq, proxiedRes) {
       newReq.end();
     }, timeout);
   }
-  if (shouldBeTimed(proxiedReq.headers.host)) {
+
+  if (shouldBeTimed(proxiedReq)) {
     al.lock(function(unlockReal){
       console.log("Lock serviced us...");
       var locked=true;
